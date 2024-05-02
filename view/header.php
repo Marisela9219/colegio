6b859efabd1dc968175d7c38c66a072f52d40f58
Marisela Escobar <76004021+Marisela9219@users.noreply.github.com>
2024-05-01 22:22:39 -0600

    Add files via upload

---
+++ view/header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<title>Anexsoft</title>
+        
+        <meta charset="utf-8" />
+        
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css" />
+        <link rel="stylesheet" href="assets/js/jquery-ui/jquery-ui.min.css" />
+        <link rel="stylesheet" href="assets/css/style.css" />
+        
+        <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+	</head>
+    <body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Inicio</a>
+           
+           <!--  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                 <li class="nav-item active">
+                    <a class="nav-link" href="#">Inicio <span class="sr-only">(current)</span></a>
+                </li>
+                </ul>
+            </div> -->
+        </nav>
+    </header>
+        
+    <div class="container">
